ba58045806d3be4f5b7aadb917246bc52e5b0f7c
Alan Reid <alan.reid@businessandtrade.gov.uk>
2026-02-23 10:50:47 +0000

    Update to packages to fix the repo

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -1,21 +1,18 @@
-import { join } from 'path'
-import { cwd } from 'process'
+import { join } from "path";
+import { cwd } from "process";
 
-import MiniCssExtractPlugin from 'mini-css-extract-plugin'
-import TerserPlugin from 'terser-webpack-plugin'
-import webpack from 'webpack'
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import TerserPlugin from "terser-webpack-plugin";
+import webpack from "webpack";
 
-const {
-  NODE_ENV = 'development',
-  PORT = 8080
-} = process.env
+const { NODE_ENV = "development", PORT = 8080 } = process.env;
 
 /**
  * Base webpack build mode
  */
-const mode = ['development', 'test'].includes(NODE_ENV)
-  ? 'development'
-  : 'production'
+const mode = ["development", "test"].includes(NODE_ENV)
+  ? "development"
+  : "production";
 
 /**
  * Base webpack config
@@ -23,79 +20,84 @@ const mode = ['development', 'test'].includes(NODE_ENV)
  * @satisfies {WebpackConfiguration}
  */
 const config = {
-  bail: mode === 'production',
-  context: join(cwd(), 'src'),
+  bail: mode === "production",
+  context: join(cwd(), "src"),
 
-  devtool: mode === 'development'
-    ? 'inline-source-map'
-    : 'source-map',
+  resolve: {
+    alias: {
+      react: "preact/compat",
+      "react-dom/test-utils": "preact/test-utils",
+      "react-dom": "preact/compat",
+      "react/jsx-runtime": "preact/jsx-runtime",
+    },
+  },
 
-  externalsType: 'umd',
+  devtool: mode === "development" ? "inline-source-map" : "source-map",
+
+  externalsType: "umd",
   mode,
 
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'postcss-loader'
-        ]
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
       },
       {
         test: /\.js$/,
-        include: join(cwd(), 'src'),
-        enforce: 'pre',
-        loader: 'source-map-loader'
+        include: join(cwd(), "src"),
+        enforce: "pre",
+        loader: "source-map-loader",
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            rootMode: 'upward'
-          }
-        }
-      }
-    ]
+            rootMode: "upward",
+          },
+        },
+      },
+    ],
   },
 
   node: {
     global: true,
     __filename: false,
-    __dirname: false
+    __dirname: false,
   },
 
   optimization: {
-    emitOnErrors: mode === 'production',
-    minimize: mode === 'production',
-    minimizer: [new TerserPlugin({
-      extractComments: true,
-      terserOptions: {
-        format: { comments: false },
+    emitOnErrors: mode === "production",
+    minimize: mode === "production",
+    minimizer: [
+      new TerserPlugin({
+        extractComments: true,
+        terserOptions: {
+          format: { comments: false },
 
-        // Include sources content from dependency source maps
-        sourceMap: {
-          includeSources: true
+          // Include sources content from dependency source maps
+          sourceMap: {
+            includeSources: true,
+          },
+
+          // Compatibility workarounds
+          safari10: true,
         },
-
-        // Compatibility workarounds
-        safari10: true
-      }
-    })]
+      }),
+    ],
   },
 
   output: {
-    path: join(cwd(), 'dist'),
-    publicPath: '/dist'
+    path: join(cwd(), "dist"),
+    publicPath: "/dist",
   },
 
   stats: {
-    colors: true
-  }
-}
+    colors: true,
+  },
+};
 
 /**
  * Bundle standalone 'accessible-autocomplete'
@@ -106,44 +108,44 @@ const bundleStandalone = {
   ...config,
 
   devServer: {
-    allowedHosts: 'all',
-    host: '0.0.0.0',
-    open: '/dist',
+    allowedHosts: "all",
+    host: "0.0.0.0",
+    open: "/dist",
     port: PORT,
     static: [
       {
-        directory: join(cwd(), 'examples'),
-        publicPath: '/dist',
-        watch: true
-      }
-    ]
+        directory: join(cwd(), "examples"),
+        publicPath: "/dist",
+        watch: true,
+      },
+    ],
   },
 
   entry: {
-    'accessible-autocomplete': {
+    "accessible-autocomplete": {
       import: [
-        join(cwd(), 'src/autocomplete.css'),
-        join(cwd(), 'src/wrapper.js')
+        join(cwd(), "src/autocomplete.css"),
+        join(cwd(), "src/wrapper.js"),
       ],
-      filename: '[name].min.js',
+      filename: "[name].min.js",
       library: {
-        export: 'default',
-        name: 'accessibleAutocomplete',
-        type: 'umd'
-      }
-    }
+        export: "default",
+        name: "accessibleAutocomplete",
+        type: "umd",
+      },
+    },
   },
 
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.COMPONENT_LIBRARY': '"PREACT"',
-      'process.env.NODE_ENV': `"${mode}"`
+      "process.env.COMPONENT_LIBRARY": '"PREACT"',
+      "process.env.NODE_ENV": `"${mode}"`,
     }),
     new MiniCssExtractPlugin({
-      filename: '[name].min.css'
-    })
-  ]
-}
+      filename: "[name].min.css",
+    }),
+  ],
+};
 
 /**
  * Bundle for Preact 'accessible-autocomplete/preact.js'
@@ -154,19 +156,19 @@ const bundlePreact = {
   ...config,
 
   entry: {
-    'accessible-autocomplete.preact': {
-      import: join(cwd(), 'src/autocomplete.js'),
-      filename: 'lib/[name].min.js',
+    "accessible-autocomplete.preact": {
+      import: join(cwd(), "src/autocomplete.js"),
+      filename: "lib/[name].min.js",
       library: {
-        name: 'Autocomplete',
-        type: 'umd',
-        umdNamedDefine: true
-      }
-    }
+        name: "Autocomplete",
+        type: "umd",
+        umdNamedDefine: true,
+      },
+    },
   },
 
   externals: {
-    preact: 'preact'
+    preact: "preact",
   },
 
   output: {
@@ -174,16 +176,16 @@ const bundlePreact = {
 
     // Support `window.preact` when not bundled
     // e.g. with all dependencies included via unpkg.com
-    globalObject: 'this'
+    globalObject: "this",
   },
 
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.COMPONENT_LIBRARY': '"PREACT"',
-      'process.env.NODE_ENV': `"${mode}"`
-    })
-  ]
-}
+      "process.env.COMPONENT_LIBRARY": '"PREACT"',
+      "process.env.NODE_ENV": `"${mode}"`,
+    }),
+  ],
+};
 
 /**
  * Bundle for React 'accessible-autocomplete/react.js'
@@ -194,24 +196,24 @@ const bundleReact = {
   ...config,
 
   entry: {
-    'accessible-autocomplete.react': {
-      import: join(cwd(), 'src/autocomplete.js'),
-      filename: 'lib/[name].min.js',
+    "accessible-autocomplete.react": {
+      import: join(cwd(), "src/autocomplete.js"),
+      filename: "lib/[name].min.js",
       library: {
-        name: 'Autocomplete',
-        type: 'umd',
-        umdNamedDefine: true
-      }
-    }
+        name: "Autocomplete",
+        type: "umd",
+        umdNamedDefine: true,
+      },
+    },
   },
 
   externals: {
     preact: {
-      amd: 'react',
-      commonjs: 'react',
-      commonjs2: 'react',
-      root: 'React'
-    }
+      amd: "react",
+      commonjs: "react",
+      commonjs2: "react",
+      root: "React",
+    },
   },
 
   output: {
@@ -219,25 +221,21 @@ const bundleReact = {
 
     // Support extending `window.React` when not bundled
     // e.g. with all dependencies included via unpkg.com
-    globalObject: 'this'
+    globalObject: "this",
   },
 
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.COMPONENT_LIBRARY': '"REACT"',
-      'process.env.NODE_ENV': `"${mode}"`
-    })
-  ]
-}
+      "process.env.COMPONENT_LIBRARY": '"REACT"',
+      "process.env.NODE_ENV": `"${mode}"`,
+    }),
+  ],
+};
 
 /**
  * Multiple webpack config export
  */
-export default [
-  bundleStandalone,
-  bundlePreact,
-  bundleReact
-]
+export default [bundleStandalone, bundlePreact, bundleReact];
 
 /**
  * @typedef {import('webpack-dev-server').WebpackConfiguration} WebpackConfiguration
